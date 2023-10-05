9dd2bc567fdd7d914f7970c5948911c1f60d6f46
Guy Sandler <guy@sandler.co.il>
2023-10-05 15:31:34 -0700

    V0.21

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,92 @@
 use std::env;
 use std::fs;
 use std::process::exit;
+use std::path::{Path, PathBuf};
 use dialoguer::{theme::ColorfulTheme, Select};
+
 fn main() {
-    // lists of menu
+    let mut current_dir = std::env::current_dir().unwrap();
+
+    loop {
+        let (mut menu, ui) = getfiles(&current_dir);
+
+        if ui {
+            let selection = Select::with_theme(&ColorfulTheme::default())
+                .default(0)
+                .items(&menu[..])
+                .interact()
+                .unwrap();
+
+            if menu[selection] == "🚪: exit".to_string() {
+                break;
+            } else if menu[selection] == "⇦: ..".to_string() {
+                if let Some(parent_dir) = current_dir.parent() {
+                    current_dir = parent_dir.to_path_buf();
+                }
+            }
+        } else {
+            menu.remove(0);
+            menu.remove(0);
+            for item in menu {
+                println!("{}", item)
+            }
+            break;
+        }
+    }
+}
+
+fn getfiles(current_dir: &Path) -> (Vec<String>, bool) {
     let mut dirs: Vec<String> = vec![];
     let mut files: Vec<String> = vec![];
-    // menu
     let mut menu: Vec<String> = vec!["🚪: exit".to_string(), "⇦: ..".to_string()];
     let mut ui: bool = true;
 
-    // getting files in dir
-    let current_dir = env::current_dir().unwrap();
     println!("Current directory: {}", current_dir.display());
-    let paths = fs::read_dir("./").unwrap();
-    for path in paths {
-        if let Ok(entry) = path {
-            if let Some(file_name) = entry.path().file_name() {
-                if entry.file_type().unwrap().is_file() {
-                    files.push(file_name.to_string_lossy().to_string());
-                } else if entry.file_type().unwrap().is_dir() {
-                    dirs.push(file_name.to_string_lossy().to_string());
+
+    if let Ok(entries) = fs::read_dir(current_dir) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if let Some(file_name) = entry.file_name().to_str() {
+                    if entry.file_type().unwrap().is_file() {
+                        files.push(format!("📄: {}", file_name));
+                    } else if entry.file_type().unwrap().is_dir() {
+                        dirs.push(format!("📁: {}", file_name));
+                    }
                 }
             }
         }
     }
 
-    // format output
     let args: Vec<String> = env::args().collect();
     if args.len() != 1 {
-        // check args
-        if args[1] == "dir" || args[1] == "fil" || args[1] == "noui" {} else {
+        if args[1] == "dir" {
+            for item in dirs {
+                menu.push(item);
+            }
+        } else if args[1] == "fil" {
+            for item in files {
+                menu.push(item);
+            }
+        } else if args[1] == "noui" {
+            ui = false;
+            for item in dirs {
+                menu.push(item);
+            }
+            for item in files {
+                menu.push(item);
+            }
+        } else {
             println!("command not found");
             exit(1);
         }
-
-        // commands
-        if args[1] == "dir" {
-            for item in dirs {
-                menu.push(format!("📁: {}", item))
-            }
-        }
-        else if args[1] == "fil" {
-            for item in files
-            {
-                menu.push(format!("📄: {}", item))
-            }
-        }
-        else if args[1] == "noui" {
-            ui = false;
-            for item in dirs {
-                menu.push(format!("📁: {}", item))
-            }
-            for item in files {
-                menu.push(format!("📄: {}", item))
-            }
-        }
-    }
-    // no command
-    else {
+    } else {
         for item in dirs {
-            menu.push(format!("📁: {}", item))
+            menu.push(item);
         }
         for item in files {
-            menu.push(format!("📄: {}", item))
+            menu.push(item);
         }
     }
-    // output format
-    if ui == true {
-        let selection = Select::with_theme(&ColorfulTheme::default())
-            .with_prompt("Pick your flavor")
-            .default(0)
-            .items(&menu[..])
-            .interact()
-            .unwrap();
-        println!("Enjoy your {}!", menu[selection]);
-    }
-    else if ui == false {
-        menu.remove(0);
-        menu.remove(0);
-        for item in menu {
-            println!("{}", item)
-        }
-    }
+
+    (menu, ui)
 }
